f76bbdaebb2c2c053239b0acd3c589ffe042d7d4
imhvost <imhvost@gmail.com>
2025-12-10 13:20:09 +0200

    localhost fix

--- theme/inc/blocks/plan.php
+++ theme/inc/blocks/plan.php
@@ -68,7 +68,7 @@ add_action(
 								<?php endif; ?>
 								<?php if ( $fields['iframe_src'] ) : ?>
 									<div class="volna-plan-iframe">
-										<?php if ( volna_is_localhost() ) : ?>
+										<?php if ( ! volna_is_localhost() ) : ?>
 											<iframe src="<?php echo esc_url( $fields['iframe_src'] ); ?>" loading='lazy'></iframe>
 										<?php endif; ?>
 									</div>
